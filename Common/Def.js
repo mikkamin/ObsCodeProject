e88cb554b7a1d62d2fb255c5c40ece1904c46824
akiyoshi.mikami_adglobe <akiyoshi.mikami@adglobe.co.jp>
2024-02-01 18:07:24 +0900

    共通定数をまとめる

---
+++ Common/Def.js
@@ -0,0 +1,13 @@
+// 共通変数
+window.DEF = window.DEF || {};
+DEF.googleApiKey = "AIzaSyBdE8WIwvVcbbLby2XVW-tm41AOt169PB8";
+
+// Youtube用変数
+window.YOUTUBEDEF = window.YOUTUBEDEF || {};
+YOUTUBEDEF.channelID = "UCpZd2ct72njFCZonzRW94iw";
+
+// OBS用変数
+window.OBSDEF = window.OBSDEF || {};
+OBSDEF.host = "192.168.10.101";
+OBSDEF.port = "4455";
+OBSDEF.password = "9xq9iEFiDYcVIRSx";
